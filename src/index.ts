aefe0ccc8c3aac4e0a7208f4189387fccaacc8eb
Carlos Matheu <info@plastikaweb.com>
2022-06-04 19:28:35 +0200

    refactor 1

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf-8'
-}).split('\n').map((row: string): string[] => row.split(','));
-
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D'
-};
+console.log(reader.data[0][0]);
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
